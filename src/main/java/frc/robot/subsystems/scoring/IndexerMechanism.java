e1dfba45bb22cb045d79ac45a231a0ca3ad35697
aidnem <99768676+aidnem@users.noreply.github.com>
2025-08-25 19:57:24 -0400

    (#5) Define scoring subsystem stubs

---
+++ src/main/java/frc/robot/subsystems/scoring/IndexerMechanism.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.scoring;
+
+public class IndexerMechanism {
+  public void indexIntoShooter() {
+    // TODO: Implement indexing into shooter
+  }
+
+  public void stopIndexing() {
+    // TODO: Implement returning indexer to zero
+  }
+}
